996bf98cc0c9ad1209d70d5c39d98a45eb781adc
Alex Staples <ajstaples@gmail.com>
2024-06-22 00:25:32 -0400

    home and projects margin adjustments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,9 @@ import UnderConstruction from "./UnderConstruction";
 export default function Home() {
 	return (
 		<div>
-			<div className="mt-48 mx-10 pb-48 text-center border-b-2">
+			<div className="mt-24 md:mt-48 mx-4 md:mx-10 pb-16 md:pb-48 text-center border-b-2">
 				<h1 className="font-heading">Hi. I'm Alex.</h1>
-				<p className="mt-6 font-body text-lg lg:mx-32">
+				<p className="mt-6 font-body text-md md:text-lg lg:mx-32">
 					I'm a software developer from Northborough, Massachusetts. I've been
 					writing code professionally since 2015, with the majority of my career
 					focused on full stack web technology. I'm especially fond of the React
@@ -13,17 +13,21 @@ export default function Home() {
 					types including eCommerce, data management, and biotechnology.
 				</p>
 			</div>
-			<div className="mt-24">
-				<h2 className="mb-6 font-bold text-xl mx-8 font-heading">PROJECTS</h2>
-				<div className="grid grid-cols:1 md:grid-cols-2 lg:grid-cols-3 gap-20 mx-10">
+			<div className="mt-16 md:mt-8">
+				<h2 className="mb-4 md:mb-6 font-bold text-xl mx-4 md:mx-8 font-heading">
+					PROJECTS
+				</h2>
+				<div className="grid grid-cols:1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-20 mx-4 md:mx-10">
 					<UnderConstruction small />
 					<UnderConstruction small />
 					<UnderConstruction small />
 				</div>
 			</div>
-			<div className="mt-24">
-				<h2 className="mb-6 font-bold text-xl mx-8 font-heading">EXPERIENCE</h2>
-				<div className="grid mx-10">
+			<div className="mt-16 md:mt-8">
+				<h2 className="mb-4 md:mb-6 font-bold text-xl mx-4 md:mx-8 font-heading">
+					EXPERIENCE
+				</h2>
+				<div className="grid mx-4 md:mx-10">
 					<div className="border-black dark:border-white border-2 flex-1 h-72 under-construction-small">
 						<div className="h-full flex justify-center items-center ">
 							<div className={"bg-yellow-300 text-black text-xl font-bold p-2"}>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import UnderConstruction from "./UnderConstruction";
 export default function Projects() {
 	return (
 		<div>
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-20 mt-24 mx-10">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-20 mt-12 md:mt-24 mx-4 md:mx-10">
 				<UnderConstruction />
 				<UnderConstruction />
 			</div>
