130a1c78bda98446ebf9448c920ea0b668c01efa
Alex Staples <ajstaples@gmail.com>
2025-09-04 13:30:18 -0400

    fix page scroll position upon route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Switch } from "wouter";
+import { useLayoutEffect } from "react";
+import { Route, Switch, useLocation } from "wouter";
 
 import Footer from "./components/Footer";
 import NavHeader from "./components/NavHeader";
@@ -11,19 +11,25 @@ import Resume from "./pages/Resume";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="">
-			<NavHeader />
-			<Switch>
-				<Route path="/" component={Home} />
-				<Route path="/projects" component={Projects} />
-				<Route path="/experience" component={Resume} />
-				<Route path="/resume" component={Resume} />
-				<Route path="/cover-letter" component={CoverLetter} />
-			</Switch>
-			<Footer />
-		</div>
-	);
+  const [location] = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [location]);
+
+  return (
+    <div className="">
+      <NavHeader />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/experience" component={Resume} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/cover-letter" component={CoverLetter} />
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
